c1de7c5fda72a5ff19ea9e1982aea5f1b617f274
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2024-04-20 23:36:11 +0200

    init project add code

---
+++ RUN_STANALONE.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe"  "C:\Users\Atlan\OneDrive\Herni-vyvoj\GalacticHavoc\GalacticHavoc.uproject" ^
+/Game/Maps/L_Intro ^
+ -game -PIEVIACONSOLE -Multiprocess GameUserSettingsINI=PIEGameUserSettings0 -MultiprocessSaveConfig ^
+ -forcepassthrough -messaging -SessionName="Play in Standalone Game" -windowed -ResX=1920 -ResY=1080
