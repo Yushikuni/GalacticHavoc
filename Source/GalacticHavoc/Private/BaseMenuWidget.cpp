ec7339f11327efc419a0988f6224814acb50ec47
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2024-05-19 14:29:45 +0200

    Making a Base widget Menu for WBPs menus

---
+++ Source/GalacticHavoc/Private/BaseMenuWidget.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseMenuWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBaseMenuWidget::OpenMenu()
+{
+	this->AddToViewport();
+}
+
+void UBaseMenuWidget::CloseMenu()
+{
+	this->RemoveFromParent();
+}
+
+void UBaseMenuWidget::QuitGame()
+{
+	//UGameplayStatics::OpenLevel(this, FName("L_MainMenu"));
+}
+
+void UBaseMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	InitializeMenu();
+}
---
+++ Source/GalacticHavoc/Public/BaseMenuWidget.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseMenuWidget.generated.h"
+
+/**
+ * This is template class for WBP Menus and screens such as Dead screen and Win screen
+ */
+UCLASS()
+class GALACTICHAVOC_API UBaseMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void OpenMenu();
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void CloseMenu();
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void QuitGame();
+
+	virtual void InitializeMenu();
+
+protected:
+	virtual void NativeConstruct() override;
+};
